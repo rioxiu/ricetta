5f1123341e71e4313c49eaf3068904ad30fe67a3
richard <richardo.aog@gmail.com>
2023-01-08 21:17:36 +0700

    commit

--- pages/meals/ingredients.js
+++ pages/meals/ingredients.js
@@ -7,7 +7,7 @@ const Ingredients = ({ Ingmeasures }) => {
       <table className="bg-slate-900 rounded w-full max-w-xl border-spacing-0 text-center">
         <tbody>
           {Ingmeasures.map((ms) => (
-            <tr key={ms.i} className="border-b-2">
+            <tr key={ms.index} className="border-b-2">
               <td className="p-2">
                 <p>{ms.ingredient}</p>
               </td>
